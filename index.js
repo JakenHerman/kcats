2bcb8faaad2ce3723ce1f8dcb4823a79fcaab843
jaken herman <jakenherman7@gmail.com>
2016-10-07 00:29:53 -0500

    added multistack functionality - all stacks allocated in one array

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
 var method = kcats.prototype;
 
-function kcats(max_len){
+function kcats(max_len, num_stacks){
     this._max_len = max_len;
-    this._top = -1;
+    this._num_stacks = num_stacks || 1;
+    this._top = new Array(this._num_stacks);
     this._k = new Array(this._max_len);
+    this._alloc = this._max_len / this._num_stacks;
+
+    for(var i = 0; i < this._max_len; i++){
+        this._top[i] = (this._alloc * i) - 1;
+    }
 }
 
-method._isEmpty = function(){
-    return (this._top == -1);
+method._isEmpty = function(top_index){
+    return (this._top[top_index] == ((this._alloc * top_index) - 1));
 };
 
-method._reset = function(){
-    this._top = -1;
+method._reset = function(top_index){
+    this._top[top_index] = ((this._alloc * top_index) -1);
 };
 
-method._isFull = function(){
-    return (this._top == this._max_len - 1);
+method._isFull = function(top_index){
+    return (this._top[top_index] == this._max_len - 1);
 };
 
-method._push = function(x){
-    if(this._top < (this._max_len - 1)){
-	this._k[++this._top] = x;
+method._push = function(x, top_index){
+    if(this._top[top_index] < (this._max_len - 1)){
+	    this._k[++this._top[top_index]] = x;
     }
     else {
-	console.log("Stack overflow, element not added.");
+	    console.log("Stack overflow, element not added.");
     }
 };
 
-method._pop = function(){
-    if(this._top != -1){
-	return this._k[this._top--];
+method._pop = function(top_index){
+    if(this._top[top_index] != ((this._alloc * top_index) -1)){
+	    return this._k[this._top[top_index]--];
+    }
+    else {
+	    return "Stack is empty";
+    }
+};
+
+method._peek = function(top_index){
+    if(this._top[top_index] != ((this._alloc * top_index) -1)){
+	return this._k[this._top[top_index]];
     }
     else {
 	return "Stack is empty";
-    }
-};
-
-method._peek = function(){
-    if(this._top != -1){
-	return this._k[this._top];
-    }
-    else {
-	return "stack is empty";
     }
 };
 
--- tests/test.js
+++ tests/test.js
@@ -1,57 +1,60 @@
 var assert = require('assert');
 var kcats = require('../index.js');
-
+  
 describe('kcats', function() {
+
   describe('#._isEmpty()', function() {
     it('Determines whether or not the stack contains anything.', function() {
-      var tests = new kcats(10);
-      var result1 = tests._isEmpty();        
+      var tests = new kcats(10, 1);
+      var result1 = tests._isEmpty(0);        
       assert.equal(result1, true);
-      tests._push(1);
-      var result2 = tests._isEmpty();
+      tests._push("hello", 0);
+      var result2 = tests._isEmpty(0);
       assert.equal(result2, false);
-      tests._reset();
-      var result3 = tests._isEmpty();
+      tests._reset(0);
+      var result3 = tests._isEmpty(0);
       assert.equal(result3, true);
     });
   });
-  
+
   describe('#._reset()', function(){
       it('Allows you to instantaneously pop all elements, without printing them.', function () {
           var tests = new kcats(10);
-          tests._push(1);
-          tests._push(2);
-          tests._push(3);
-          var isEmptyFalse = tests._isEmpty();
+          tests._push("mocha", 0);
+          tests._push("test", 0);
+          tests._push("stack", 0);
+          var isEmptyFalse = tests._isEmpty(0);
           assert.equal(isEmptyFalse, false);
-          tests._reset(); //main reason for test
-          var isEmptyTrue = tests._isEmpty();
+          tests._reset(0); //main reason for test
+          var isEmptyTrue = tests._isEmpty(0);
           assert.equal(isEmptyTrue, true);
       });
   });
+
+
 
   describe('#._isFull()', function() {
       it('Determines whether or not the stack is full.', function() {
           var tests = new kcats(10);
           for(var i = 0; i < 11; i++){
-              tests._push(i);
+              tests._push(i, 0);
           }
-          var isFull = tests._isFull();
+          var isFull = tests._isFull(0);
           assert.equal(isFull, true);
-          tests._reset();
-          var isNotFull = tests._isFull();
+          tests._reset(0);
+          var isNotFull = tests._isFull(0);
           assert.equal(isNotFull, false);
       });
   });
 
   describe('#._push()', function() {
       it('Allows you to add an item to the stack.', function(){
-          var tests = new kcats(3);
-          tests._push(1);
-          var isCorrectValue = tests._peek(); // should be 1
+          var tests = new kcats(3, 2);
+          tests._push(1, 0);
+          var isCorrectValue = tests._peek(0); // should be 1
           assert.equal(isCorrectValue, 1);
-          tests._push(2);
-          var isCorrectValue = tests._peek(); // should be 2
+          tests._push(2, 1);
+          var isCorrectValue = tests._peek(1); // should be 2
           assert.equal(isCorrectValue, 2);
       });
   });
@@ -59,10 +62,10 @@ describe('kcats', function() {
   describe('#._pop()', function() {
       it('Allows you to retrieve the element at the top of the stack.', function() {
           var tests = new kcats(3);
-          tests._push(6);
-          tests._push(42);
-          var is42 = tests._pop();
-          var is6 = tests._pop();
+          tests._push(6, 0);
+          tests._push(42, 0);
+          var is42 = tests._pop(0);
+          var is6 = tests._pop(0);
           assert.equal(is42, 42);
           assert.equal(is6, 6);
       });
@@ -74,14 +77,13 @@ describe('kcats', function() {
           var tests = new kcats(3);
           tests._push(453);
           tests._push(652);
-          var is_652_peek = tests._peek();
-          var is_652_pop = tests._pop();
+          var is_652_peek = tests._peek(0);
+          var is_652_pop = tests._pop(0);
           assert.equal(is_652_peek, is_652_pop);
-          var is_453_peek = tests._peek();
-          var is_453_pop = tests._peek();
+          var is_453_peek = tests._peek(0);
+          var is_453_pop = tests._peek(0);
           assert.equal(is_453_peek, is_453_pop);
-      })
-  })
-
+      });
+  });
 });
 
